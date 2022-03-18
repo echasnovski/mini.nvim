2b0b2eb1c5e5db1c58a565c3712f68152876fc78
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2022-03-18 09:34:49 +0200

    (mini.statusline) Add note about using new Neovim's global statusline.
    Details:
    - neovim/neovim#17266 .

--- README.md
+++ README.md
@@ -620,7 +620,9 @@ Default `config`:
     inactive = nil,
   },
 
-  -- Whether to set Vim's settings for statusline (make it always shown)
+  -- Whether to set Vim's settings for statusline (make it always shown with
+  -- 'laststatus' set to 2). To use global statusline in Neovim>=0.7.0, set
+  -- this to `false` and 'laststatus' to 3.
   set_vim_settings = true,
 }
 ```
--- lua/mini/statusline.lua
+++ lua/mini/statusline.lua
@@ -168,7 +168,9 @@ MiniStatusline.config = {
     inactive = nil,
   },
 
-  -- Whether to set Vim's settings for statusline (make it always shown)
+  -- Whether to set Vim's settings for statusline (make it always shown with
+  -- 'laststatus' set to 2). To use global statusline in Neovim>=0.7.0, set
+  -- this to `false` and 'laststatus' to 3.
   set_vim_settings = true,
 }
 --minidoc_afterlines_end
