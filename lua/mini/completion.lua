08252d0d742b55fd2ac4b07fa1c9cb485322e99c
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2022-04-15 12:15:24 +0300

    (mini.completion) Document common mapping for `<CR>`.
    Details:
    - Resolves #62.

--- lua/mini/completion.lua
+++ lua/mini/completion.lua
@@ -39,6 +39,8 @@
 --- What it doesn't do:
 --- - Snippet expansion.
 --- - Many configurable sources.
+--- - Automatic mapping of `<CR>`, `<Tab>`, etc., as those tend to have highly
+---   variable user expectations. See 'Helpful key mappings' for suggestions.
 ---
 --- # Setup~
 ---
@@ -73,9 +75,32 @@
 ---
 --- To use `<Tab>` and `<S-Tab>` for navigation through completion list, make
 --- these key mappings:
---- `vim.api.nvim_set_keymap('i', [[<Tab>]],   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { noremap = true, expr = true })`
---- `vim.api.nvim_set_keymap('i', [[<S-Tab>]], [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { noremap = true, expr = true })`
+--- `vim.api.nvim_set_keymap('i', '<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { noremap = true, expr = true })`
+--- `vim.api.nvim_set_keymap('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { noremap = true, expr = true })`
 ---
+--- To get more consistent behavior of `<CR>`, you can use this template in
+--- your 'init.lua' to make customized mapping: >
+---   local keys = {
+---     ['cr']        = vim.api.nvim_replace_termcodes('<CR>', true, true, true),
+---     ['ctrl-y']    = vim.api.nvim_replace_termcodes('<C-y>', true, true, true),
+---     ['ctrl-y_cr'] = vim.api.nvim_replace_termcodes('<C-y><CR>', true, true, true),
+---   }
+---
+---   _G.cr_action = function()
+---     if vim.fn.pumvisible() ~= 0 then
+---       -- If popup is visible, confirm selected item or add new line otherwise
+---       local item_selected = vim.fn.complete_info()['selected'] ~= -1
+---       return item_selected and keys['ctrl-y'] or keys['ctrl-y_cr']
+---     else
+---       -- If popup is not visible, use plain `<CR>`. You might want to customize
+---       -- according to other plugins. For example, to use 'mini.pairs', replace
+---       -- next line with `return require('mini.pairs').cr()`
+---       return keys['cr']
+---     end
+---   end
+---
+---   vim.api.nvim_set_keymap('i', '<CR>', 'v:lua._G.cr_action()', { noremap = true, expr = true })
+--- <
 --- # Highlight groups~
 ---
 --- 1. `MiniCompletionActiveParameter` - highlighting of signature active
