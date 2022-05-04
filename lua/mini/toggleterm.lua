26679cfc677f4f24ebe45c1a219dfc7d269c191a
Crag <ogeidca49@gmail.com>
2022-05-04 09:16:48 -0500

    fix minor error

--- lua/mini/toggleterm.lua
+++ lua/mini/toggleterm.lua
@@ -5,12 +5,12 @@ local terminal_opened_win_id = -1
 local M = {}
 local config = {
   prefix = "",
-  pos = "bot"
+  pos = "bot",
   size = 20
 }
 
 function M.setup(user_config)
-  config = vim.tbl_deep_extend("force", config, user_config)
+  config = vim.tbl_deep_extend("force", config, user_config or {})
   config.prefix = string.format("%s %d new ", config.pos, config.size)
 end
 
@@ -52,3 +52,5 @@ function M.MiniTermSend(cmd)
     vim.fn.jobsend(jobid, cmd .. "\n")
   end
 end
+
+return M
