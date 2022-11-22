d00abe8169993b95f52ff64fbfe685f353fd1c4f
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2022-11-22 10:10:11 +0200

    Update script for dual pushing.

--- scripts/dual_push.sh
+++ scripts/dual_push.sh
@@ -4,6 +4,12 @@ local_repos="$( ls -d dual/repos/*/ )"
 for repo in $local_repos; do
   printf "\n\033[1mPushing $( basename $repo )\033[0m\n"
   cd $repo > /dev/null
-  git push origin main
+  # Push only if there is something to push (saves time)
+  if [ $( git rev-parse main ) != $( git rev-parse origin/main ) ]
+  then
+    git push origin main
+  fi
   cd - > /dev/null
 done
+
+echo ''
