229b12cb2375ceb8412fb7529de92d9e4ca28b62
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2023-03-25 20:50:24 +0200

    (mini.test tests) Don't test child process redirection of `treesitter`.
    Details:
    - This is mostly a reaction of moving `list_directives` to submodule in
      Neovim (commit cbbf8bd).

--- tests/test_test.lua
+++ tests/test_test.lua
@@ -1002,7 +1002,8 @@ T['child']['redirected method tables'] = new_set({
     { 'lsp', 'get_active_clients', {} },
     { 'mpack', 'encode', { { a = 1 } } },
     { 'spell', 'check', { 'thouht' } },
-    { 'treesitter', 'list_directives', {} },
+    -- The `treesitter` module is also redirected but there is no reliable way
+    -- to test it without installing parsers
   },
 })
 
