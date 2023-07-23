d1da5dba30f82bc2a5f94ba3483d8c35b7f3a6fe
Dr. David A. Kunz <david.kunz@sap.com>
2023-07-23 11:39:58 +0200

    include options

--- lua/mini/files.lua
+++ lua/mini/files.lua
@@ -800,11 +800,20 @@ MiniFiles.close = function()
 end
 
 
---- Toggle explorer
+--- Toggle file explorer
 ---
-MiniFiles.toggle = function()
+---@param path string|nil A valid file system path used as anchor.
+---   If it is a path to directory, used directly.
+---   If it is a path to file, its parent directory is used as anchor while
+---   explorer will focus on the supplied file.
+---   Default: path of |current-directory|.
+---@param use_latest boolean|nil Whether to load explorer state from history
+---   (based on the supplied anchor path). Default: `true`.
+---@param opts table|nil Table of options overriding |MiniFiles.config| and
+---   `vim.b.minifiles_config` for this particular explorer session.
+MiniFiles.toggle = function(path, use_latest, opts)
   local explorer = H.explorer_get()
-  if explorer == nil then MiniFiles.open() else MiniFiles.close() end
+  if explorer == nil then MiniFiles.open(path, use_latest, opts) else MiniFiles.close() end
 end
 
 --- Go in entry under cursor
