c2352031124e90cac9d038cb8a1f1b406f7465ea
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2024-10-11 15:43:30 +0300

    fix(pairs): update `cr()` to not trigger `TextChanged`
    It is an event describing text change in Normal mode, which it seems to
    also be triggered during `<C-o>`.
    
    Resolve #1273

--- lua/mini/pairs.lua
+++ lua/mini/pairs.lua
@@ -395,7 +395,7 @@ MiniPairs.cr = function(key)
     -- Temporarily ignore mode change to not trigger some common expensive
     -- autocommands (like diagnostic check, etc.)
     local cache_eventignore = vim.o.eventignore
-    vim.o.eventignore = 'InsertLeave,InsertLeavePre,InsertEnter,ModeChanged'
+    vim.o.eventignore = 'InsertLeave,InsertLeavePre,InsertEnter,TextChanged,ModeChanged'
     H.restore_eventignore(cache_eventignore)
 
     -- Temporarily redraw lazily for no cursor flicker due to `<C-o>O`.
