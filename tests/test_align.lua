echasnovski/mini.nvim

commit b83127c36cc7a9f3cb9e16f5d9db1231d30c33f9

fix(align): update `gen_step.default_merge` to not affect indent
Resolve #1684

lua/mini/align.lua
@@ -1119,7 +1119,8 @@ MiniAlign.gen_step.default_justify = function() return MiniAlign.new_step('justi
 --- Outline of how parts are converted to array of strings:
 --- - Convert `merge_delimiter` option to array of strings (single string is
 ---   converted as one-element array). Recycle this array to have length equal
----   to number of columns in parts minus 1.
+---   to number of columns in parts minus 1. Also possibly trim leading whitespace
+---   in first merge character to not affect indentation.
 --- - Exclude empty strings from parts. They add nothing to output except extra
 ---   usage of merge delimiter.
 --- - Concatenate each row interleaving with array of merge delimiters.
@@ -1606,11 +1607,14 @@ H.default_action_merge = function(parts, opts)
     delimiter_arr[j] = H.slice_mod(delimiter, j)
   end
 
-  -- Concat non-empty cells (empty cells at this point add only extra merge)
-  return vim.tbl_map(function(row)
-    local row_no_empty = vim.tbl_filter(function(s) return s ~= '' end, row)
-    return H.concat_array(row_no_empty, delimiter_arr)
-  end, parts)
+  -- Do not change indentation
+  local first_parts_are_indent = true
+  for i = 1, dims.row do
+    first_parts_are_indent = first_parts_are_indent and H.is_whitespace(parts[i][1])
+  end
+  delimiter_arr[1] = first_parts_are_indent and delimiter_arr[1]:gsub('^%s*', '') or delimiter_arr[1]
+
+  return vim.tbl_map(function(row) return H.concat_array(row, delimiter_arr) end, parts)
 end
 
 -- Work with modifiers --------------------------------------------------------
@@ -1992,8 +1996,11 @@ end
 H.concat_array = function(target_arr, concat_arr)
   local ext_arr = {}
   for i = 1, #target_arr - 1 do
-    table.insert(ext_arr, target_arr[i])
-    table.insert(ext_arr, concat_arr[i])
+    -- Concat non-empty cells (empty cells at this point add only extra merge)
+    if target_arr[i] ~= '' then
+      table.insert(ext_arr, target_arr[i])
+      table.insert(ext_arr, concat_arr[i])
+    end
   end
   table.insert(ext_arr, target_arr[#target_arr])
   return table.concat(ext_arr, '')

tests/test_align.lua
@@ -968,6 +968,23 @@ T['gen_step']['default_merge()']['does not merge empty strings in parts'] = func
   validate_align_strings({ '=a' }, { merge_delimiter = '-' }, { '=-a' })
 end
 
+T['gen_step']['default_merge()']['preserves indentation with whitespace in merge delimmiter'] = function()
+  set_config_opts({ split_pattern = '=' })
+
+  -- Should not add whitespace to the first part if all of them are indent
+  validate_align_strings({ ' =a=b', '  =c=d' }, { merge_delimiter = ' ' }, { '  = a = b', '  = c = d' })
+  validate_align_strings({ ' =a=b', '=c=d' }, { merge_delimiter = ' ' }, { ' = a = b', ' = c = d' })
+  validate_align_strings({ '=a=b', '=c=d' }, { merge_delimiter = ' ' }, { '= a = b', '= c = d' })
+
+  validate_align_strings({ ' =a=b', '  =c=d' }, { merge_delimiter = ' _' }, { '  _= _a _= _b', '  _= _c _= _d' })
+
+  validate_align_strings({ ' =a=b', 'x=c=d' }, { merge_delimiter = ' ' }, { '  = a = b', 'x = c = d' })
+
+  child.o.tabstop = 2
+  validate_align_strings({ '\t=a=b', '\t\t=c=d' }, { merge_delimiter = '\t' }, { '\t  =\ta\t=\tb', '\t\t=\tc\t=\td' })
+  validate_align_strings({ '\t=a=b', '\t\t=c=d' }, { merge_delimiter = ' ' }, { '\t  = a = b', '\t\t= c = d' })
+end
+
 T['gen_step']['filter()'] = new_set()
 
 T['gen_step']['filter()']['works'] = function()

commit 51b2fca19e7e3c87aad33690db5c872c547bdc67

fix(align): handle empty list and strings properly
This fixes a bug introduced in b83127c36cc7a9f3cb9e16f5d9db1231d30c33f9.

lua/mini/align.lua
@@ -1617,11 +1617,13 @@ H.default_action_merge = function(parts, opts)
   end
 
   -- Do not change indentation
-  local first_parts_are_indent = true
-  for i = 1, dims.row do
-    first_parts_are_indent = first_parts_are_indent and H.is_whitespace(parts[i][1])
+  if #delimiter_arr > 0 then
+    local first_parts_are_indent = true
+    for i = 1, dims.row do
+      first_parts_are_indent = first_parts_are_indent and H.is_whitespace(parts[i][1])
+    end
+    delimiter_arr[1] = first_parts_are_indent and delimiter_arr[1]:gsub('^%s*', '') or delimiter_arr[1]
   end
-  delimiter_arr[1] = first_parts_are_indent and delimiter_arr[1]:gsub('^%s*', '') or delimiter_arr[1]
 
   return vim.tbl_map(function(row) return H.concat_array(row, delimiter_arr) end, parts)
 end

tests/test_align.lua
@@ -953,6 +953,13 @@ T['gen_step']['default_merge()']['works'] = function()
   )
 end
 
+T['gen_step']['default_merge()']['indentity for empty list and zero-length strings'] = function()
+  validate_align_strings({}, {}, {})
+  validate_align_strings({ '' }, {}, { '' })
+  validate_align_strings({ '', '' }, {}, { '', '' })
+  validate_align_strings({ '', ' ' }, {}, { '', ' ' })
+end
+
 T['gen_step']['default_merge()']['verifies relevant options'] = function()
   expect.error(
     function() child.lua([[MiniAlign.align_strings({ 'a' }, { merge_delimiter = 1 }, {})]]) end,